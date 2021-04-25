f779dbf2dc79de2184e00e386516e0913f9d8780
Martin Eldredge <martin.eldredge1@gmail.com>
2021-04-25 17:22:21 -0600

    add navbar

--- server.py
+++ server.py
@@ -4,3 +4,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
 	return render_template("index.html")
+
+@app.route('/docs')
+def docs():
+	return render_template("docs.html")
+
+@app.route('/mail')
+def mail():
+	return render_template("mail.html")
