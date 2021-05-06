be865d7f3ee8f947d8d31be87ed9175678fb4b5e
Martin Eldredge <martin.eldredge1@gmail.com>
2021-05-06 15:22:50 -0600

    format Email.js

--- src/Email.js
+++ src/Email.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
 function Email() {
-  return (
-    <div>
-        <h1>E-Mail</h1>
-        <p>Welcome to the E-Mail page</p>
-    </div>
-  );
+    return (
+        <div>
+            <h1>E-Mail</h1>
+            <p>Welcome to the E-Mail page</p>
+        </div>
+    );
 }
 
 export default Email;
